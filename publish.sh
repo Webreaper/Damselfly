4c6e02fb5e21116a7b03fc93f9db88f3fb0137af
Mark Otway <mark@otway.com>
2020-12-07 15:31:27 +0000

    Non-docker builds/packages

--- publish.sh
+++ publish.sh
@@ -12,4 +12,24 @@ dotnet publish Damselfly.Web -r osx-x64 -c Release --self-contained true /p:Publ
 dotnet publish Damselfly.Web -r linux-x64 -c Release --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=true /p:Version=$version /p:IncludeNativeLibrariesForSelfExtract=true
 dotnet publish Damselfly.Web -r win-x64 -c Release --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=true /p:Version=$version /p:IncludeNativeLibrariesForSelfExtract=true
 
+mkdir server
+rm damselfly-for-mac.zip
+rm damselfly-for-windows.zip
+rm damselfly-for-linux.zip
+
+pushd .
+cd Damselfly.Web/bin/Release/net5.0/osx-x64/publish
+zip -r ../../../../../../server/damselfly-for-mac.zip *.*
+popd 
+
+pushd .
+cd Damselfly.Web/bin/Release/net5.0/win-x64/publish
+zip -r ../../../../../../server/damselfly-for-windows.zip *.*
+popd
+
+pushd .
+cd Damselfly.Web/bin/Release/net5.0/linux-x64/publish
+zip -r ../../../../../../server/damselfly-for-linux.zip *.* 
+popd
+
 echo "Non-docker Damselfly build complete."
