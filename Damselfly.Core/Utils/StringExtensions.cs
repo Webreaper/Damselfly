33157f5481ccef73aab5982c3e05049c778992dc
Mark Otway <mark@otway.com>
2020-12-22 14:39:15 +0000

    Comments

--- Damselfly.Core/Utils/StringExtensions.cs
+++ Damselfly.Core/Utils/StringExtensions.cs
@@ -5,40 +5,38 @@ namespace Damselfly.Core.Utils
 {
     public static class StringExtensions
     {
+        /// <summary>
+        /// Case insensitive contains. Still not really sure why this
+        /// isn't something that's part of .Net....
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="toCheck"></param>
+        /// <returns></returns>
         public static bool ContainsNoCase(this string source, string toCheck)
         {
             bool passes = source?.IndexOf(toCheck, StringComparison.OrdinalIgnoreCase) >= 0;
             return passes;
         }
 
-        public static string ToHumanReadableString(this long timeMilliSecs)
-        {
-            int x = (int)timeMilliSecs;
-            return x.ToHumanReadableString();
-        }
-
+        /// <summary>
+        /// Display a date/time in a format which is locale-agnostic
+        /// and looks tidy in the UI. Wrapped in a helper so I can
+        /// change this format in a single place if need be.
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
         public static string Display(this DateTime date)
         {
             return $"{date:dd-MMM-yyy HH:mm:ss}";
         }
 
-        public static string ToHumanReadableString(this DateTime start )
-        {
-            return start.ToHumanReadableString(DateTime.UtcNow);
-
-        }
-        public static string ToHumanReadableString(this DateTime start, DateTime end )
-        {
-            int milliSeconds = (int)((end - start).TotalMilliseconds);
-            return milliSeconds.ToHumanReadableString();
-        }
-
-        public static string ToHumanReadableString(this int timeMilliSecs)
-        {
-            var t = new TimeSpan(0, 0, 0, 0, timeMilliSecs);
-            return t.ToHumanReadableString();
-        }
-
+        /// <summary>
+        /// Creates a human-readable string for a timespan, rounding to the
+        /// nearest significant order of magnitude. So something like
+        /// "5 seconds", or "2 hours".
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
         public static string ToHumanReadableString(this TimeSpan t)
         {
             int totalWeeks = (int)(t.TotalDays / 7);
@@ -73,6 +71,29 @@ namespace Damselfly.Core.Utils
             }
 
             return $@"{t:s\.ff}ms";
+        }
+
+        public static string ToHumanReadableString(this long timeMilliSecs)
+        {
+            int x = (int)timeMilliSecs;
+            return x.ToHumanReadableString();
+        }
+
+        public static string ToHumanReadableString(this DateTime start)
+        {
+            return start.ToHumanReadableString(DateTime.UtcNow);
+
+        }
+        public static string ToHumanReadableString(this DateTime start, DateTime end)
+        {
+            int milliSeconds = (int)((end - start).TotalMilliseconds);
+            return milliSeconds.ToHumanReadableString();
+        }
+
+        public static string ToHumanReadableString(this int timeMilliSecs)
+        {
+            var t = new TimeSpan(0, 0, 0, 0, timeMilliSecs);
+            return t.ToHumanReadableString();
         }
 
         /// <summary>
