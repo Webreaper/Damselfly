b63c4708a8c9eb4d830c7dd447fd04b28ada2fb4
Mark Otway <mark@otway.com>
2021-01-20 12:47:15 +0000

    Node modules in container

--- makedesktop.sh
+++ makedesktop.sh
@@ -18,7 +18,7 @@ docker run --rm  \
  --env ELECTRON_CACHE="/root/.cache/electron" \
  --env ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" \
  -v ${PWD}:/project \
- -v ${PWD##*/}-node-modules:/project/node_modules \
+# -v ${PWD##*/}-node-modules:/project/node_modules \
  -v ~/.cache/electron:/root/.cache/electron \
  -v ~/.cache/electron-builder:/root/.cache/electron-builder \
  electronuserland/builder:wine  \
