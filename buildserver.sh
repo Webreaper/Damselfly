ca3cb65c80343dbbb9fe8df8abf5e01f24d3caf0
Mark Otway <mark@otway.com>
2021-01-20 22:40:06 +0000

    Update buildserver.sh

--- buildserver.sh
+++ buildserver.sh
@@ -1,5 +1,7 @@
 # Read the version from disk.
 version=`cat VERSION`
 
+echo Building Server for $1 v$version
+
 dotnet publish Damselfly.Web -r $1 -c Release --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=true /p:Version=$version /p:IncludeNativeLibrariesForSelfExtract=true
 
