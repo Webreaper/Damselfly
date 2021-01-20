dd2ca19fac64f0ed904fc593fb51bbff93ad2d55
Mark Otway <mark@otway.com>
2021-01-20 17:04:16 +0000

    Exclusions

--- makeserver.sh
+++ makeserver.sh
@@ -21,17 +21,17 @@ dotnet publish Damselfly.Web -r win-x64 -c Release --self-contained true /p:Publ
 
 echo Zipping OSX Server...
 cd Damselfly.Web/bin/Release/net5.0/osx-x64/publish
-zip -r ../../../../../../server/damselfly-server-mac-$version.zip *.* -x ".pdb" 
+zip -r ../../../../../../server/damselfly-server-mac-$version.zip *.* -x "*.pdb" 
 cd ../../../../../..
 
 echo Zipping Windows Server...
 cd Damselfly.Web/bin/Release/net5.0/win-x64/publish
-zip -r ../../../../../../server/damselfly-server-windows-$version.zip *.* -x ".pdb" 
+zip -r ../../../../../../server/damselfly-server-windows-$version.zip *.* -x "*.pdb" 
 cd ../../../../../..
 
 echo Zipping Linux Server...
 cd Damselfly.Web/bin/Release/net5.0/linux-x64/publish
-zip -r ../../../../../../server/damselfly-server-linux-$version.zip *.* -x ".pdb" 
+zip -r ../../../../../../server/damselfly-server-linux-$version.zip *.* -x "*.pdb" 
 cd ../../../../../..
 
 pwd
