1b4ac8bdf93dac3c9bff45664085b4676a8ca5a0
Mark Otway <mark@otway.com>
2021-01-20 16:50:17 +0000

    Strip PDBs

--- makeserver.sh
+++ makeserver.sh
@@ -8,23 +8,33 @@ fi
 
 echo "**************************** Building Non-Docker Damselfly ****************************"
 
+mkdir server
+
+echo Building OSX Server...
 dotnet publish Damselfly.Web -r osx-x64 -c Release --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=true /p:Version=$version /p:IncludeNativeLibrariesForSelfExtract=true
+
+echo Building Linux Server...
 dotnet publish Damselfly.Web -r linux-x64 -c Release --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=true /p:Version=$version /p:IncludeNativeLibrariesForSelfExtract=true
+
+echo Building Windows Server...
 dotnet publish Damselfly.Web -r win-x64 -c Release --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=true /p:Version=$version /p:IncludeNativeLibrariesForSelfExtract=true
 
-mkdir server
-rm server/*.*
-
+echo Zipping OSX Server...
 cd Damselfly.Web/bin/Release/net5.0/osx-x64/publish
-zip -r ../../../../../../server/damselfly-server-mac-$version.zip *.*
+zip -r -x ".pdb" ../../../../../../server/damselfly-server-mac-$version.zip *.*
 cd ../../../../../..
 
+echo Zipping Windows Server...
 cd Damselfly.Web/bin/Release/net5.0/win-x64/publish
-zip -r ../../../../../../server/damselfly-server-windows-$version.zip *.*
+zip -r -x ".pdb" ../../../../../../server/damselfly-server-windows-$version.zip *.*
 cd ../../../../../..
 
+echo Zipping Linux Server...
 cd Damselfly.Web/bin/Release/net5.0/linux-x64/publish
-zip -r ../../../../../../server/damselfly-server-linux-$version.zip *.* 
+zip -r -x ".pdb" ../../../../../../server/damselfly-server-linux-$version.zip *.* 
 cd ../../../../../..
+
+pwd
+ls server
 
 echo "Non-docker Damselfly build complete."
