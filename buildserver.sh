d7cedaf51d0fa47bb2b2b1477aa841aaa9c0791b
Mark Otway <mark@otway.com>
2021-01-20 23:07:31 +0000

    Update buildserver.sh

--- buildserver.sh
+++ buildserver.sh
@@ -1,7 +1,11 @@
 # Read the version from disk.
 version=`cat VERSION`
 
-echo Building Server for $1 v$version
+echo Building Server for $1 v$version (zip: $2)
 
 dotnet publish Damselfly.Web -r $1 -c Release --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=true /p:Version=$version /p:IncludeNativeLibrariesForSelfExtract=true
+
+cd $GITHUB_WORKSPACE/Damselfly.Web/bin/Release/net5.0/$1/publish
+mkdir $GITHUB_WORKSPACE/server
+zip -r $GITHUB_WORKSPACE/server/$2-$version.zip *.* -x "*.pdb" 
 
