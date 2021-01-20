2745caae82567058847e09424d8c6d57662ec1d7
Mark Otway <mark@otway.com>
2021-01-20 12:48:29 +0000

    Docker run

--- makedesktop.sh
+++ makedesktop.sh
@@ -13,12 +13,13 @@ echo "**************************** Building Desktop Electron Apps **************
 
 env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_' > docker_env
 
+# -v ${PWD##*/}-node-modules:/project/node_modules \
+
 docker run --rm  \
  --env-file docker_env \
  --env ELECTRON_CACHE="/root/.cache/electron" \
  --env ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" \
  -v ${PWD}:/project \
-# -v ${PWD##*/}-node-modules:/project/node_modules \
  -v ~/.cache/electron:/root/.cache/electron \
  -v ~/.cache/electron-builder:/root/.cache/electron-builder \
  electronuserland/builder:wine  \
