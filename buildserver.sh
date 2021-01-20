53cc361b19bc0284405e0172b6c21bdad56e4618
Mark Otway <mark@otway.com>
2021-01-20 23:15:18 +0000

    Logs

--- buildserver.sh
+++ buildserver.sh
@@ -5,7 +5,10 @@ echo Building Server for $1 into $2-$version.zip
 
 dotnet publish Damselfly.Web -r $1 -c Release --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=true /p:Version=$version /p:IncludeNativeLibrariesForSelfExtract=true
 
+echo Zipping build to [$GITHUB_WORKSPACE/server/$2-$version.zip]...
 cd $GITHUB_WORKSPACE/Damselfly.Web/bin/Release/net5.0/$1/publish
 mkdir $GITHUB_WORKSPACE/server
 zip -r $GITHUB_WORKSPACE/server/$2-$version.zip *.* -x "*.pdb" 
+ls $GITHUB_WORKSPACE/server
 
+echo Build complete.
