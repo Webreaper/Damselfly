8e0190b36921247d8dae0e7e1ed59ad7681d3208
Mark Otway <mark@otway.com>
2021-01-21 11:37:53 +0000

    String

--- damselfly-entrypoint.sh
+++ damselfly-entrypoint.sh
@@ -10,7 +10,7 @@ echo "Increasing inotify watch limit..."
 echo fs.inotify.max_user_instances=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 
 echo "Preparing to start Damselfly...."
-echo "  ./Damselfly.Web /pictures --config=/config ${cmdlineargs}
+echo "  ./Damselfly.Web /pictures --config=/config ${cmdlineargs}"
 
 ./Damselfly.Web /pictures --config=/config ${cmdlineargs}
 
