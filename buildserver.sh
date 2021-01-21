3656542db0a6d16051240439edff07f211b68b30
Mark Otway <mark@otway.com>
2021-01-21 00:25:44 +0000

    Nofind

--- buildserver.sh
+++ buildserver.sh
@@ -7,8 +7,6 @@ dotnet publish Damselfly.Web -r $1 -c Release --self-contained true /p:PublishSi
 
 echo Zipping build to [$GITHUB_WORKSPACE/server/$2-$version.zip]...
 cd $GITHUB_WORKSPACE/Damselfly.Web/bin/Release/net5.0/$1/publish
-find . -type f
-
 mkdir $GITHUB_WORKSPACE/server
 zip $GITHUB_WORKSPACE/server/$2-$version.zip . -rx "*.pdb" 
 ls $GITHUB_WORKSPACE/server
