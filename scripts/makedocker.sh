e793c0c2d0a4f4b380e3ff04e5e865a4b3e2b5d9
Mark Otway <mark@otway.com>
2021-01-25 23:45:15 +0000

    Automatic release....!?

--- scripts/makedocker.sh
+++ scripts/makedocker.sh
@@ -1,8 +1,18 @@
 
+if [ -n "$1" ]; then
+    version=`cat VERSION`
+    DOCKERTAG="${version}-beta"
+else
+    echo 'No docker tag specified. Defaulting to dev'
+    DOCKERTAG='dev'
+fi
+
 echo "**** Building Docker Damselfly"
 docker build -t damselfly . 
 
-docker tag damselfly webreaper/damselfly:dev
-docker push webreaper/damselfly:dev
+echo "*** Pushing docker image to ${DOCKERTAG}"
+
+docker tag damselfly webreaper/damselfly:$DOCKERTAG
+docker push webreaper/damselfly:$DOCKERTAG
 
 echo "Damselfly docker build complete."
