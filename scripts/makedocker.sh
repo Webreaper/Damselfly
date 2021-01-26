69caddc73def3e6743ffe5b5fc2a68dc7c91db1e
Mark Otway <mark@otway.com>
2021-01-26 00:09:09 +0000

    Tidy up names and asset wildcards

--- scripts/makedocker.sh
+++ scripts/makedocker.sh
@@ -1,18 +1,19 @@
 
 if [ -n "$1" ]; then
+    echo 'No docker tag specified. Pushing to dev'
+    DOCKERTAG='dev'
+else
     version=`cat VERSION`
     DOCKERTAG="${version}-beta"
-else
-    echo 'No docker tag specified. Defaulting to dev'
-    DOCKERTAG='dev'
+    echo "Pushing to docker tag: ${DOCKERTAG}"
 fi
 
 echo "**** Building Docker Damselfly"
 docker build -t damselfly . 
 
-echo "*** Pushing docker image to ${DOCKERTAG}"
+echo "*** Pushing docker image to webreaper/damselfly:${DOCKERTAG}"
 
-docker tag damselfly webreaper/damselfly:$DOCKERTAG
-docker push webreaper/damselfly:$DOCKERTAG
+# docker tag damselfly webreaper/damselfly:$DOCKERTAG
+# docker push webreaper/damselfly:$DOCKERTAG
 
 echo "Damselfly docker build complete."
