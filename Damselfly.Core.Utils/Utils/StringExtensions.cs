21dca791d01eb532abda1b83e1b600f87b8c86dc
Mark Otway <mark@otway.com>
2021-05-10 22:08:09 +0100

    Don't display invalid DateTaken dates

--- Damselfly.Core.Utils/Utils/StringExtensions.cs
+++ Damselfly.Core.Utils/Utils/StringExtensions.cs
@@ -21,12 +21,16 @@ namespace Damselfly.Core.Utils
         /// <summary>
         /// Display a date/time in a format which is locale-agnostic
         /// and looks tidy in the UI. Wrapped in a helper so I can
-        /// change this format in a single place if need be.
+        /// change this format in a single place if need be. If the
+        /// date is invalid, display unknown.
         /// </summary>
         /// <param name="date"></param>
         /// <returns></returns>
         public static string Display(this DateTime date)
         {
+            if (date == DateTime.MinValue)
+                return "Unknown";
+
             return $"{date:dd-MMM-yyy HH:mm:ss}";
         }
         /// <summary>
