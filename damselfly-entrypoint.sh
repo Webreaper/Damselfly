42e8a3dfbb310b3fe994e7138239cb54a8642bdd
Mark Otway <mark@otway.com>
2021-06-28 10:40:19 +0100

    Refactor JS

--- damselfly-entrypoint.sh
+++ damselfly-entrypoint.sh
@@ -12,8 +12,6 @@ echo fs.inotify.max_user_instances=524288 | sudo tee -a /etc/sysctl.conf && sudo
 echo "Preparing to start Damselfly...."
 echo "  ./Damselfly.Web /pictures --config=/config --thumbs=/thumbs ${cmdlineargs}"
 
-LD_DEBUG=libs ./Damselfly.Web
-
 ./Damselfly.Web /pictures --config=/config --thumbs=/thumbs ${cmdlineargs}
 
 exec "$@"
