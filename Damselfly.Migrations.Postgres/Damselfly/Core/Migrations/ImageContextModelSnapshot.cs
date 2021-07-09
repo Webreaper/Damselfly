c3f77d6e0685eed76b3984d7f700fcaec488e039
Mark Otway <mark@otway.com>
2021-07-09 17:17:47 +0100

    Fix migrations build

--- Damselfly.Migrations.Postgres/Damselfly/Core/Migrations/ImageContextModelSnapshot.cs
+++ Damselfly.Migrations.Postgres/Damselfly/Core/Migrations/ImageContextModelSnapshot.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Damselfly.Core.Migrations
 {
--- Damselfly.Migrations.Postgres/Migrations/20210624220109_AddMLTypes.Designer.cs
+++ Damselfly.Migrations.Postgres/Migrations/20210624220109_AddMLTypes.Designer.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Damselfly.Core.Migrations
 {
