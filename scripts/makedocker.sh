d94d14c986fee2f5cc9439f06c9aeb9f9823f1f5
Mark Otway <mark@otway.com>
2021-07-14 13:31:52 +0100

    Add then

--- scripts/makedocker.sh
+++ scripts/makedocker.sh
@@ -17,6 +17,7 @@ docker tag damselfly webreaper/damselfly:$DOCKERTAG
 docker push webreaper/damselfly:$DOCKERTAG
 
 if [ -n "$1" ]
+then
     echo "*** Pushing docker image to webreaper/damselfly:latest"
     docker tag damselfly webreaper/damselfly:latest
     docker push webreaper/damselfly:latest
