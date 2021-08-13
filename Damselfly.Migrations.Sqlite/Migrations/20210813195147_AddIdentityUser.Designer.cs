0775e9c263cd218b087958de139b13a70518e168
Mark Otway <mark@otway.com>
2021-08-13 20:53:36 +0100

    Add int-keyed identity user

--- Damselfly.Migrations.Sqlite/Migrations/20210813195147_AddIdentityUser.Designer.cs
+++ Damselfly.Migrations.Sqlite/Migrations/20210813195147_AddIdentityUser.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Damselfly.Core.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Damselfly.Core.Migrations
 {
     [DbContext(typeof(ImageContext))]
-    partial class ImageContextModelSnapshot : ModelSnapshot
+    [Migration("20210813195147_AddIdentityUser")]
+    partial class AddIdentityUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Damselfly.Migrations.Sqlite/Migrations/20210813195147_AddIdentityUser.cs
@@ -0,0 +1,248 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Damselfly.Core.Migrations
+{
+    public partial class AddIdentityUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "KeywordOperations",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OwnerId",
+                table: "ConfigSettings",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "ConfigSettings",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OwnerId",
+                table: "Baskets",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "AspNetUserTokens",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RoleId",
+                table: "AspNetUserRoles",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "AspNetUserRoles",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "AspNetUserLogins",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "AspNetUserClaims",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "AspNetRoles",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RoleId",
+                table: "AspNetRoleClaims",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KeywordOperations_UserId",
+                table: "KeywordOperations",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConfigSettings_OwnerId",
+                table: "ConfigSettings",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Baskets_OwnerId",
+                table: "Baskets",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Baskets_AspNetUsers_OwnerId",
+                table: "Baskets",
+                column: "OwnerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ConfigSettings_AspNetUsers_OwnerId",
+                table: "ConfigSettings",
+                column: "OwnerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_KeywordOperations_AspNetUsers_UserId",
+                table: "KeywordOperations",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Baskets_AspNetUsers_OwnerId",
+                table: "Baskets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ConfigSettings_AspNetUsers_OwnerId",
+                table: "ConfigSettings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_KeywordOperations_AspNetUsers_UserId",
+                table: "KeywordOperations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_KeywordOperations_UserId",
+                table: "KeywordOperations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ConfigSettings_OwnerId",
+                table: "ConfigSettings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Baskets_OwnerId",
+                table: "Baskets");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "KeywordOperations");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "ConfigSettings");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "ConfigSettings");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Baskets");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "AspNetUserTokens",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RoleId",
+                table: "AspNetUserRoles",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "AspNetUserRoles",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "AspNetUserLogins",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "AspNetUserClaims",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "AspNetRoles",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RoleId",
+                table: "AspNetRoleClaims",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+    }
+}
