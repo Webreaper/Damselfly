0ede2372163bd11aea11c2db0e06d7939fb0261a
Mark Otway <mark@otway.com>
2021-10-06 08:55:56 +0100

    Clean up migration

--- Damselfly.Migrations.Sqlite/Migrations/20211005112145_RemoveHashColumn.cs
+++ Damselfly.Migrations.Sqlite/Migrations/20211005112145_RemoveHashColumn.cs
@@ -9,7 +9,6 @@ namespace Damselfly.Core.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            /*
             Logging.Log("Migrating hashes to new table...");
             // Copy data
             const string sql = @"INSERT INTO Hashes (ImageId, MD5ImageHash)
@@ -17,66 +16,20 @@ namespace Damselfly.Core.Migrations
                                 WHERE i.ImageID not in
                                     (SELECT imageid FROM hashes);";
             migrationBuilder.Sql(sql);
-            Logging.Log("Hash migration complete. Dropping column...");
-            */
+
             migrationBuilder.DropColumn(
                 name: "Hash",
                 table: "ImageMetaData");
-
-            Logging.Log("Column dropped.");
-
-            migrationBuilder.UpdateData(
-                table: "Roles",
-                keyColumn: "Id",
-                keyValue: 1,
-                column: "ConcurrencyStamp",
-                value: "82b60d90-9f24-4a12-a246-bc6e1f529102");
-
-            migrationBuilder.UpdateData(
-                table: "Roles",
-                keyColumn: "Id",
-                keyValue: 2,
-                column: "ConcurrencyStamp",
-                value: "adc5f5b1-cc1c-4827-abe8-0c7c5fdbbc68");
-
-            migrationBuilder.UpdateData(
-                table: "Roles",
-                keyColumn: "Id",
-                keyValue: 3,
-                column: "ConcurrencyStamp",
-                value: "3d168dfe-134e-4802-a6c0-923f450b99c9");
-
-            Logging.Log("Migration complete.");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            Logging.Log("Calling migration Down()...");
             migrationBuilder.AddColumn<string>(
                 name: "Hash",
                 table: "ImageMetaData",
                 type: "TEXT",
                 nullable: true);
-
-            migrationBuilder.UpdateData(
-                table: "Roles",
-                keyColumn: "Id",
-                keyValue: 1,
-                column: "ConcurrencyStamp",
-                value: "fa3a79e1-2a02-4fde-b26a-1498bd911931");
-
-            migrationBuilder.UpdateData(
-                table: "Roles",
-                keyColumn: "Id",
-                keyValue: 2,
-                column: "ConcurrencyStamp",
-                value: "f9bb3be3-1fb4-4a1a-bbcb-1d71ca3e198c");
-
-            migrationBuilder.UpdateData(
-                table: "Roles",
-                keyColumn: "Id",
-                keyValue: 3,
-                column: "ConcurrencyStamp",
-                value: "e2f1656c-d1eb-4db3-b3a0-efffab6ff171");
         }
     }
 }
