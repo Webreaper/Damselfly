aa31b0c37a7ee2bfe964405a2184909fcae89a5a
Mark Otway <mark@otway.com>
2022-08-15 13:14:07 +0100

    Two errors left.

---
+++ Damselfly.Web/Components/MudNoAutofill.cs
@@ -0,0 +1,9 @@
+﻿
+using System.Collections.Generic;
+
+namespace Damselfly.Web.Components;
+
+public static class MudNoAutofill
+{
+    public static Dictionary<string, object> noAutoFillAttr = new Dictionary<string, object> { { "autocomplete", "new-password" } };
+}
