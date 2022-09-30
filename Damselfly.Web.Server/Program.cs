5ea3c17e5a2647af989ea7dcd7595447744eacea
Mark Otway <mark@otway.com>
2022-09-30 20:51:29 +0100

    Commas

--- Damselfly.Web.Server/Program.cs
+++ Damselfly.Web.Server/Program.cs
@@ -13,6 +13,7 @@ using Damselfly.Core.ScopedServices.ClientServices;
 using Damselfly.Core.Services;
 using Damselfly.Core.Utils;
 using Damselfly.Shared.Utils;
+using Damselfly.Web.Server;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
@@ -26,12 +27,6 @@ namespace Damselfly.Web;
 
 public class Program
 {
-    /// <summary>
-    ///     Can't see this being used much around the web so let's
-    ///     stake a claim for 6363 as our default port. :)
-    /// </summary>
-    private static readonly int s_defaultPort = 6363;
-
     public static void Main(string[] args)
     {
         try
@@ -280,41 +275,5 @@ public class Program
             });
 
         services.AddAuthorization(config => config.SetupPolicies(services));
-    }
-
-    public class DamselflyOptions
-    {
-        [Value(0, MetaName = "Source Directory", HelpText = "Base folder for photographs.", Required = true)]
-        public string SourceDirectory { get; set; }
-
-        [Option("config", HelpText = "Config path", Required = false)]
-        public string ConfigPath { get; set; } = "./config";
-
-        [Option("thumbs", HelpText = "Thumbnail cache path (ignored if --syno specified)", Required = false)]
-        public string ThumbPath { get; set; } = "./config/thumbs";
-
-        [Option('v', "verbose", HelpText = "Run logging in Verbose Mode")]
-        public bool Verbose { get; set; }
-
-        [Option('t', "trace", HelpText = "Enable Trace logging mode")]
-        public bool Trace { get; set; }
-
-        [Option('r', "readonly", HelpText = "Enable Read-Only mode for database")]
-        public bool ReadOnly { get; set; }
-
-        [Option("port", HelpText = "Port for Webserver (default = 6363)", Required = false)]
-        public int Port { get; set; } = s_defaultPort;
-
-        [Option("syno", Required = false, HelpText = "Use native Synology thumbnail structure.")]
-        public bool Synology { get; set; }
-
-        [Option("nothumbs", Required = false, HelpText = "Don't Generate thumbnails")]
-        public bool NoGenerateThumbnails { get; set; }
-
-        [Option("noindex", Required = false, HelpText = "Don't Index images")]
-        public bool NoEnableIndexing { get; set; }
-
-        [Option("postgres", Required = false, HelpText = "Use Postgres DB (default == Sqlite)")]
-        public bool UsePostgresDB { get; set; }
     }
 }
