a507dc78130fd8d6ca99ad2d84f5240658a67fbe
Mark Otway <mark@otway.com>
2022-10-07 15:45:34 +0100

    Update detector

---
+++ Damselfly.Web.Client/wwwroot/sw-registrator.js
@@ -0,0 +1,34 @@
+﻿window.updateAvailable = new Promise((resolve, reject) => {
+    if (!('serviceWorker' in navigator)) {
+        const errorMessage = `This browser doesn't support service workers`;
+        console.error(errorMessage);
+        reject(errorMessage);
+        return;
+    }
+
+    navigator.serviceWorker.register('/service-worker.js')
+        .then(registration => {
+            console.info(`Service worker registration successful (scope: ${registration.scope})`);
+
+            registration.onupdatefound = () => {
+                const installingServiceWorker = registration.installing;
+                installingServiceWorker.onstatechange = () => {
+                    if (installingServiceWorker.state === 'installed') {
+                        resolve(!!navigator.serviceWorker.controller);
+                    }
+                }
+            };
+        })
+        .catch(error => {
+            console.error('Service worker registration failed with error:', error);
+            reject(error);
+        });
+});
+
+window.registerForUpdateAvailableNotification = (caller, methodName) => {
+    window.updateAvailable.then(isUpdateAvailable => {
+        if (isUpdateAvailable) {
+            caller.invokeMethodAsync(methodName).then();
+        }
+    });
+};
