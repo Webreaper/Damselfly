c92195b98369641efe5df87bf3ebca079b18297d
Mark Otway <mark@otway.com>
2022-11-22 10:59:20 +0000

    Improve version stamping

--- scripts/makeserver.sh
+++ scripts/makeserver.sh
@@ -1,9 +1,6 @@
 # Read the version from disk.
 version=`cat VERSION`
 dotnetversion='7.0'
-date=$(date '+%Y%m%d%H%M%S')
-
-echo "const CACHE_VERSION='${version}-${date}'" > Damselfly.Web.Client/wwwroot/version.js
 
 if [ -n "$1" ]; then
     PLATFORM=$1
