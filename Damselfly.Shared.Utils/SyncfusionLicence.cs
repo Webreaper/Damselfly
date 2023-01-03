d25a03faeaa687bc8ebe0434933377215266c4ef
Mark Otway <mark@otway.com>
2023-01-03 10:33:08 +0000

    Dep updates

--- Damselfly.Shared.Utils/SyncfusionLicence.cs
+++ Damselfly.Shared.Utils/SyncfusionLicence.cs
@@ -7,6 +7,6 @@ public static class SyncfusionLicence
     public static void RegisterSyncfusionLicence()
     {
         SyncfusionLicenseProvider.RegisterLicense(
-            "NzU4MTU3QDMyMzAyZTMzMmUzMEEyTEJYL0I4R2RrOHdhUzJPdmh6bDJ5ZHo5bnNzNTRkYmZVRHBiMG5vdXc9");
+            "ODYyODg2QDMyMzAyZTM0MmUzMGlpc1JFVnJySjlhR0l6ZVFYK3hYR05iM1pSK3BDN093QmRndVFoYW91MVU9" );
     }
 }
