8c052f6708067200773db654d2a3c2a092af7622
Mark Otway <mark@otway.com>
2023-04-03 17:09:53 +0100

    Deps and serviceworker skip

--- Damselfly.Web.Client/wwwroot/service-worker.published.js
+++ Damselfly.Web.Client/wwwroot/service-worker.published.js
@@ -14,6 +14,7 @@ const offlineAssetsExclude = [ /^service-worker\.js$/ ];
 async function onInstall(event) {
     console.info('Service worker: Install');
 
+
     // Fetch and cache all matching items from the assets manifest
     const assetsRequests = self.assetsManifest.assets
         .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
@@ -28,6 +29,8 @@ async function onInstall(event) {
 
 async function onActivate(event) {
     console.info('Service worker: Activate');
+
+    self.skipWaiting();
 
     // Delete unused caches
     const cacheKeys = await caches.keys();
