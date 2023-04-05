a3e710bba3d0f7abce593b0921fb860599775829
Mark Otway <mark@otway.com>
2023-04-05 20:50:56 +0100

    Be less aggressive about ExifOperation cleanup

--- Damselfly.Core/Services/ExifService.cs
+++ Damselfly.Core/Services/ExifService.cs
@@ -551,8 +551,8 @@ public class ExifService : IProcessJobFactory, ITagService
         using var scope = _scopeFactory.CreateScope();
         using var db = scope.ServiceProvider.GetService<ImageContext>();
 
-        // Clean up completed operations older than 24hrs
-        var cutOff = DateTime.UtcNow.AddDays(-1);
+        // Clean up completed operations older than 6 months
+        var cutOff = DateTime.UtcNow.AddMonths(-6);
 
         try
         {
