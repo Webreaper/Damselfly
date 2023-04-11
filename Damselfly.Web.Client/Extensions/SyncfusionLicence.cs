a8e59d2b13b907ba6cc71a49b18f03ae2a76d710
Mark Otway <mark@otway.com>
2023-04-11 20:30:36 +0100

    Deps and licence key

--- Damselfly.Web.Client/Extensions/SyncfusionLicence.cs
+++ Damselfly.Web.Client/Extensions/SyncfusionLicence.cs
@@ -7,6 +7,6 @@ public static class SyncfusionLicence
     public static void RegisterSyncfusionLicence()
     {
         SyncfusionLicenseProvider.RegisterLicense(
-            "ODYyODg2QDMyMzAyZTM0MmUzMGlpc1JFVnJySjlhR0l6ZVFYK3hYR05iM1pSK3BDN093QmRndVFoYW91MVU9" );
+            "MTY5MDQ5NUAzMjMxMmUzMTJlMzMzOUJBZk85WWJSZXRXK1NudHJWUExMeGk3Y0lUeFNRUXFUQm9ReGxtMmhYOXc9" );
     }
 }
