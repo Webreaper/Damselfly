d18517a0bef9ac5d09511721295517553e937c3c
Mark Otway <mark@otway.com>
2023-07-25 08:44:00 +0100

    Basic rotation

---
+++ Damselfly.Core.DbModels/Models/TransformationModels/Mono.cs
@@ -0,0 +1,7 @@
+
+namespace Damselfly.Core.DbModels.Models.TransformationModels;
+
+public class MonoTransform : ITransform
+{
+    public int Order => 3;
+}
--- Damselfly.Web.Server/Controllers/ImageController.cs
+++ Damselfly.Web.Server/Controllers/ImageController.cs
@@ -101,7 +101,7 @@ public class ImageController : Controller
                     var gotThumb = true;
 
 
-                    if ( !System.IO.File.Exists(imagePath) )
+                    if ( !System.IO.File.Exists(imagePath) || image.MetaData.ThumbLastUpdated == null )
                     {
                         gotThumb = false;
                         Logging.LogTrace($" - Generating thumbnail on-demand for {image.FileName}...");
