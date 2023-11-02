cd4205ac297c4d6eb8e0366eee60309110e9cee8
Mark Otway <mark@otway.com>
2023-11-02 11:44:46 +0000

    Logging

--- Damselfly.Core/ScopedServices/UserTagFavouritesService.cs
+++ Damselfly.Core/ScopedServices/UserTagFavouritesService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Damselfly.Core.Constants;
 using Damselfly.Core.ScopedServices.Interfaces;
 using Damselfly.Core.Services;
+using Microsoft.Extensions.Logging;
 
 namespace Damselfly.Core.ScopedServices;
 
@@ -13,11 +14,13 @@ public class UserTagRecentsService : IRecentTagService, IDisposable
     private readonly IConfigService _configService;
     private readonly ExifService _exifService;
     private readonly List<string> recentTags = new();
+    private readonly ILogger<UserTagRecentsService> _logger;
 
-    public UserTagRecentsService(ExifService exifService, IConfigService configService)
+    public UserTagRecentsService(ExifService exifService, IConfigService configService, ILogger<UserTagRecentsService> logger)
     {
         _configService = configService;
         _exifService = exifService;
+        _logger = logger;
 
         _exifService.OnUserTagsAdded += AddRecentTags;
 
@@ -55,25 +58,32 @@ public class UserTagRecentsService : IRecentTagService, IDisposable
     /// <param name="recentTags"></param>
     public async void AddRecentTags( ICollection<string> newRecents)
     {
-        if( newRecents == null || ! newRecents.Any() )
-            return;
-
-        const int maxRecents = 5;
-
-        var faves = await _exifService.GetFavouriteTags();
-        var recents = await GetRecentTags();
-
-        if( recents != null && recents.Any() )
+        try
         {
-            var newRecent = newRecents.Concat(recentTags)
-                .Except(faves.Select(x => x.Keyword))
-                .Distinct()
-                .Take(maxRecents).ToList();
-            recentTags.Clear();
-            recentTags.AddRange(newRecent);
+            if( newRecents == null || !newRecents.Any() )
+                return;
 
-            _configService.Set(ConfigSettings.RecentTags, string.Join(",", recentTags));
-            NotifyRecentsChanged();
+            const int maxRecents = 5;
+
+            var faves = await _exifService.GetFavouriteTags();
+            var recents = await GetRecentTags();
+
+            if( recents != null && recents.Any() )
+            {
+                var newRecent = newRecents.Concat( recentTags )
+                    .Except( faves.Select( x => x.Keyword ) )
+                    .Distinct()
+                    .Take( maxRecents ).ToList();
+                recentTags.Clear();
+                recentTags.AddRange( newRecent );
+
+                _configService.Set( ConfigSettings.RecentTags, string.Join( ",", recentTags ) );
+                NotifyRecentsChanged();
+            }
+        }
+        catch( Exception ex )
+        {
+            _logger.LogError( $"Unable to add items to recent tags list: {ex.Message}." );
         }
     }
 }
