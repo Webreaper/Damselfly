d7487755876bb5783028d61555e5d5855aa83ad8
Mark Otway <mark@otway.com>
2024-03-04 22:24:00 +0000

    First stab at FaceONNX integration

---
+++ Damselfly.ML.FaceONNX/Embeddings.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using UMapx.Core;
+
+namespace FaceEmbeddingsClassification;
+
+/// <summary>
+/// Defines the embeddings database.
+/// </summary>
+public class Embeddings
+{
+    /// <summary>
+    /// Initializes the embeddings database.
+    /// </summary>
+    public Embeddings()
+    {
+        Vectors = new List<float[]>();
+        Labels = new List<string>();
+    }
+
+    /// <summary>
+    /// Initializes the embeddings database.
+    /// </summary>
+    /// <param name="vectors">Vectors</param>
+    /// <param name="labels">Labels</param>
+    public Embeddings(List<float[]> vectors, List<string> labels)
+    {
+        if (vectors.Count != labels.Count)
+            throw new Exception("Input embedding vectors and labels must be same sizes.");
+
+        Vectors = vectors;
+        Labels = labels;
+    }
+
+    /// <summary>
+    /// Adds embedding to embeddings database.
+    /// </summary>
+    /// <param name="vector">Vector</param>
+    /// <param name="label">Label</param>
+    public void Add(float[] vector, string label)
+    {
+        Vectors.Add(vector);
+        Labels.Add(label);
+    }
+
+    /// <summary>
+    /// Removes embedding from embeddings database.
+    /// </summary>
+    /// <param name="index">Index</param>
+    public void Remove(int index)
+    {
+        Vectors.RemoveAt(index);
+        Labels.RemoveAt(index);
+    }
+
+    /// <summary>
+    /// Removes embedding from embeddings database.
+    /// </summary>
+    /// <param name="label">Label</param>
+    public void Remove(string label)
+    {
+        int index = Labels.IndexOf(label);
+
+        if (index != -1)
+        {
+            Remove(index);
+        }
+        else
+        {
+            throw new Exception("Embedding with selected label does not exist.");
+        }
+    }
+
+    /// <summary>
+    /// Clears embeddings database.
+    /// </summary>
+    public void Clear()
+    {
+        Vectors.Clear();
+        Labels.Clear();
+    }
+
+    /// <summary>
+    /// Returns embeddings database count.
+    /// </summary>
+    public int Count
+    {
+        get
+        {
+            return Vectors.Count;
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets vectors.
+    /// </summary>
+    public List<float[]> Vectors { get; }
+
+    /// <summary>
+    /// Gets or sets labels.
+    /// </summary>
+    public List<string> Labels { get; }
+
+    /// <summary>
+    /// Score vector from database by Euclidean distance.
+    /// </summary>
+    /// <param name="vector">Vector</param>
+    /// <returns>Label</returns>
+    public (string, float) FromDistance(float[] vector)
+    {
+        var length = Count;
+        var min = float.MaxValue;
+        var index = -1;
+
+        // do job
+        for (var i = 0; i < length; i++)
+        {
+            var d = Vectors[i].Euclidean(vector);
+
+            if (d < min)
+            {
+                index = i;
+                min = d;
+            }
+        }
+
+        // result
+        var label = index != -1 ? Labels?[index] : string.Empty;
+        return (label, min);
+    }
+
+    /// <summary>
+    /// Score vector from database by cosine similarity.
+    /// </summary>
+    /// <param name="vector">Vector</param>
+    /// <returns>Label and value</returns>
+    public (string, float) FromSimilarity(float[] vector)
+    {
+        var length = Vectors.Count;
+        var max = float.MinValue;
+        var index = -1;
+
+        // do job
+        for (var i = 0; i < length; i++)
+        {
+            var d = Vectors[i].Cosine(vector);
+
+            if (d > max)
+            {
+                index = i;
+                max = d;
+            }
+        }
+
+        // result
+        var label = index != -1 ? Labels?[index] : string.Empty;
+        return (label, max);
+    }
+
+}
---
+++ Damselfly.ML.FaceONNX/FaceONNXService.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Damselfly.Core.Constants;
+using Damselfly.Core.Interfaces;
+using Damselfly.Core.ScopedServices.Interfaces;
+using Damselfly.Core.Utils;
+using Damselfly.Shared.Utils;
+using FaceEmbeddingsClassification;
+using FaceONNX;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace Damselfly.ML.FaceONNX;
+
+public class FaceONNXService : IDisposable
+{
+    private readonly IConfigService _configService;
+
+    private FaceDetector _faceDetector;
+    private FaceLandmarksExtractor _faceLandmarksExtractor;
+    private FaceEmbedder _faceEmbedder;
+    private Embeddings _embeddings;
+
+    public FaceONNXService(IConfigService configService)
+    {
+        _configService = configService;
+    }
+    
+    public void Dispose()
+    {
+        _faceDetector.Dispose();
+        _faceLandmarksExtractor.Dispose();
+        _faceEmbedder.Dispose();
+    }
+
+    /// <summary>
+    ///     Initialise the face training service
+    /// </summary>
+    /// <returns></returns>
+    public async Task StartService()
+    {
+        try
+        {
+            // TODO: Load the faces from the DB and populate the embeddings
+            _faceDetector = new FaceDetector();
+            _faceLandmarksExtractor = new FaceLandmarksExtractor();
+            _faceEmbedder = new FaceEmbedder();
+            _embeddings = new Embeddings();
+        }
+        catch ( Exception ex )
+        {
+            Logging.LogError($"Unable to start Azure service: {ex.Message}");
+            if ( ex.InnerException != null )
+                Logging.LogError($"Inner exception: {ex.InnerException}");
+        }
+    }
+
+    private class FaceONNXFace
+    {
+        public string? PersonId { get; set; }
+        public Rectangle Rectangle { get; set; }
+        public float[] Embeddings { get; set; }
+    }
+    
+    private IEnumerable<FaceONNXFace> GetFacesFromImage(Image<Rgb24> image)
+    {
+        var array = new []
+        {
+            new float [image.Height,image.Width],
+            new float [image.Height,image.Width],
+            new float [image.Height,image.Width]
+        };            
+            
+        image.ProcessPixelRows(pixelAccessor =>
+        {
+            for ( var y = 0; y < pixelAccessor.Height; y++ )
+            {
+                var row = pixelAccessor.GetRowSpan(y);
+                for(var x = 0; x < pixelAccessor.Width; x++ )
+                {
+                    array[0][y, x] = row[x].R;
+                    array[1][y, x] = row[x].G;
+                    array[2][y, x] = row[x].B;
+                }
+            }
+        });
+
+        var detectResults = _faceDetector.Forward(array);
+
+        foreach( var face in detectResults )
+        {
+            if ( !face.Box.IsEmpty )
+            {
+                // landmarks
+                var points = _faceLandmarksExtractor.Forward(array, face.Box);
+                var angle = points.GetRotationAngle();
+
+                // alignment
+                var aligned = FaceLandmarksExtractor.Align(array, face.Box, angle);
+                yield return new FaceONNXFace
+                {
+                    Embeddings = _faceEmbedder.Forward(aligned),
+                    Rectangle = new Rectangle(face.Box.X, face.Box.Y, face.Box.Width, face.Box.Height)
+                };
+            }
+        }
+    }
+    
+    /// <summary>
+    ///     Detect faces
+    /// </summary>
+    /// <param name="image"></param>
+    /// <returns></returns>
+    public async Task<List<Face>> DetectFaces(Image<Rgb24> image, IImageProcessor imageProcessor)
+    {
+        var faces = new List<Face>();
+        
+        var watch = new Stopwatch("AzureFace");
+
+        try
+        {
+            var detectedFaces = GetFacesFromImage(image);
+
+            foreach( var face in detectedFaces )
+            {
+                // For each result, loop through and see if we have a match
+                var proto = _embeddings.FromSimilarity(face.Embeddings);
+                var label = proto.Item1;
+                var similarity = proto.Item2;
+
+                if( similarity > 0.75 )
+                {
+                    // Looks like we have a match. Yay!
+                    face.PersonId = label;
+                }
+                else
+                {
+                    // No match, so create a new person GUID
+                    face.PersonId = Guid.NewGuid().ToString();
+                    // Add it to the embeddings DB
+                    _embeddings.Add( face.Embeddings, face.PersonId);
+                }
+            }
+        }
+        catch ( Exception ex )
+        {
+            Logging.LogError($"Exception during FaceONNX face detection: {ex}");
+        }
+
+        watch.Stop();
+
+        if ( faces.Any() )
+            Logging.Log($"  FaceONNAX Detected {faces.Count()} faces in {watch.ElapsedTime}ms");
+        
+        return faces;
+    }
+    
+    public class Face
+    {
+        public Guid? PersonId { get; set; }
+        public int Left { set; get; }
+        public int Top { set; get; }
+        public int Width { set; get; }
+        public int Height { set; get; }
+    }
+}
