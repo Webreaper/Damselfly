db4b44632216ea5b1ab10f800f4e81fc40a8de0a
Mark Otway <mark@otway.com>
2024-04-16 22:02:53 +0100

    Progress message

--- Damselfly.Core.DbModels/Models/Entities/UserFolderState.cs
+++ Damselfly.Core.DbModels/Models/Entities/UserFolderState.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text.Json.Serialization;
-
+﻿
 namespace Damselfly.Core.Models;
 
 public class UserFolderState
