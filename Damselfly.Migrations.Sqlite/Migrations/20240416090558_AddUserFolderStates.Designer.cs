91aa9c059345d8adaad3afe857754d0aeaadaf18
Mark Otway <mark@otway.com>
2024-04-16 10:09:15 +0100

    Migration

--- Damselfly.Migrations.Sqlite/Migrations/20240416090558_AddUserFolderStates.Designer.cs
+++ Damselfly.Migrations.Sqlite/Migrations/20240416090558_AddUserFolderStates.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Damselfly.Core.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Damselfly.Core.Migrations
 {
     [DbContext(typeof(ImageContext))]
-    partial class ImageContextModelSnapshot : ModelSnapshot
+    [Migration("20240416090558_AddUserFolderStates")]
+    partial class AddUserFolderStates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.4");
