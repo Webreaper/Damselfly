5848cf689a634ba1ad7b8e568fc00e133dff4301
Mark Otway <mark@otway.com>
2024-04-16 14:09:02 +0100

    Default folders to expanded.

--- Damselfly.Core.DbModels/Models/Entities/UserFolderState.cs
+++ Damselfly.Core.DbModels/Models/Entities/UserFolderState.cs
@@ -11,5 +11,5 @@ public class UserFolderState
 {
     public int FolderId { get; set; }
     public int UserId { get; set; }
-    public bool Expanded { get; set; } = false;
+    public bool Expanded { get; set; } = true; // Always default folders to 'expanded'
 }
